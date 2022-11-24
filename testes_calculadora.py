7a13c2c355176cc5116de4ee27b42d056acc6f7b
Othávio Ruddá da Cunha Araújo <48054798+orudda@users.noreply.github.com>
2022-11-23 23:26:19 -0300

    Update testes_calculadora.py
    false and true fiedd

--- testes_calculadora.py
+++ testes_calculadora.py
@@ -4,11 +4,11 @@ from calculadora import Calculadora
 class TestCalculadora(unittest.TestCase):
   def test_dominio_divisao_igual_zero(self):
     calculadora = Calculadora(0, 0)
-    self.assertRaises(Exception, calculadora._get_times(true))
+    self.assertRaises(Exception, calculadora._get_times(True))
     
   def test_get_faixa_textual_nao_isento(self):
       calculadora = Calculadora(1, 2)
-      self.assertEqual(3, calculadora._get_sum(false))
+      self.assertEqual(3, calculadora._get_sum(False))
 
 
 

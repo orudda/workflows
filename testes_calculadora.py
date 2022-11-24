3657f70c305e68f6551a617a58b58804d16cbfcb
Othávio Ruddá da Cunha Araújo <48054798+orudda@users.noreply.github.com>
2022-11-23 23:23:38 -0300

    Create testes_calculadora.py
    testes adicionados

---
+++ testes_calculadora.py
@@ -0,0 +1,16 @@
+import unittest
+from calculadora import Calculadora
+
+class TestCalculadora(unittest.TestCase):
+  def test_dominio_divisao_igual_zero(self):
+    calculadora = Calculadora(0, 0)
+    self.assertRaises(Exception, calculadora._get_times(true))
+    
+  def test_get_faixa_textual_nao_isento(self):
+      calculadora = Calculadora(1, 2)
+      self.assertEqual(3, calculadora._get.sum(false))
+
+
+
+if __name__ == '__main__':
+    unittest.main()

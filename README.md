101bd96ab4afaf1790afab4552a5d3e1e135a3a5
orudda <othavio.rudda.cc@gmail.com>
2022-11-23 23:49:47 -0300

    testes added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # workflows
+# Aula Prática 6 - GitHub Actions
+## Objetivo
+Utilizar e explorar a ferramenta de CI/CD GitHub Actions.
+
+## Descrição
+1. Criar programa + testes: Implemente um pequeno programa de software (na
+linguagem de sua escolha) e escreva alguns testes de unidade.
+2. Utilizar ferramenta de CI/CD: Configure a ferramenta de CI/CD GitHub Actions para
+executar os testes automaticamente a cada commit no GitHub. No GitHub, você
+encontra diversos exemplos sobre como utilizar o GitHub Actions para várias linguagens.
+3. Rodar CI/CD nos SOs Ubuntu, MacOS e Windows: Configure o seu workflow do
+GitHub Actions para testar o programa em três sistemas operacionais: Ubuntu, MacOS e
+Windows.
--- testes_calculadora.py
+++ testes_calculadora.py
@@ -2,13 +2,34 @@ import unittest
 from calculadora import Calculadora
 
 class TestCalculadora(unittest.TestCase):
-  def test_dominio_divisao_igual_zero(self):
-    calculadora = Calculadora(0, 0)
-    self.assertRaises(Exception, calculadora._get_times(True))
+  # def test_dominio_divisao_igual_zero(self):
+  #   calculadora = Calculadora(0, 0)
+  #   self.assertRaises(Exception, calculadora._get_times(True))
     
-  def test_get_faixa_textual_nao_isento(self):
-      calculadora = Calculadora(1, 2)
-      self.assertEqual(3, calculadora._get_sum(False))
+  def test_subtraction_result(self):
+    calculadora = Calculadora(1, 2)
+    self.assertEqual(-1, calculadora._get_sum(False))
+
+  def test_sum_result(self):
+    calculadora = Calculadora(1, 2)
+    self.assertEqual(3, calculadora._get_sum(True))
+
+  def test_mod_btw_2_values(self):
+    calculadora = Calculadora(5, 2)
+    self.assertEqual(1, calculadora._get_mod())
+
+  def test_pot_btw_2_values(self):
+    calculadora = Calculadora(5, 2)
+    self.assertEqual(25, calculadora._get_pot())
+
+  def test_times_result(self):
+    calculadora = Calculadora(6, 2)
+    self.assertEqual(3, calculadora._get_times(True))
+
+  def test_times_result(self):
+    calculadora = Calculadora(6, 2)
+    self.assertEqual(12, calculadora._get_times(False))
+
 
 
 
